c472b3865cc593cb91b79fce93502927ee15e556
BGW <104225231+klbgw@users.noreply.github.com>
2022-04-25 22:49:16 +0300

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,5 @@
+sudo pacman -S gdm gnome gnome-extra gnome-tweaks adwaita-icon-theme arc-gtk-theme arc-icon-theme eog evince file-roller gedit gnome-calculator gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome-themes-extra gnome-usage gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus nautilus-sendto xdg-user-dirs-gtk firefox
+
+sudo systemctl enable gdm
+sleep 5
+sudo reboot
