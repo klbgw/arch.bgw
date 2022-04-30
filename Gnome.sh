153ac3859701fbbe3c86efb16b3dd2dfb141cdde
BGW <104225231+klbgw@users.noreply.github.com>
2022-04-30 18:52:38 +0300

    Add files via upload

---
+++ Gnome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo reflector -c Russian,Germany,France -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+sudo pacman -S --noconfirm adwaita-icon-theme arc-gtk-theme-eos eos-qogir-icons eog evince file-roller firefox gdm gedit gnome-calculator gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome themes-extra gnome-tweaks gnome-usage gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs nautilus nautilus-sendto sushi totem xdg-user-dirs-gtk
+sudo systemctl enable gdm
+sleep 5
+sudo reboot
