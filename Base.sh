36842137517d89a6fc7c2ab0099774045c620bba
BGW <104225231+klbgw@users.noreply.github.com>
2023-07-16 15:06:03 +0300

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-pacman -Sy
-pacman -S adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji accountsservice acpid adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts alsa-firmware alsa-plugins alsa-utils avahi bash-completion bluez bluez-utils broadcom-wl-dkms cantarell-fonts dhclient dhcpcd dialog dina-font dmidecode dmraid dnsmasq dnsutils duf edk2-ovmf efibootmgr efitools ethtool f2fs-tools ffmpegthumbnailer firewalld freetype2 fsarchiver gentium-plus-font glances gnu-free-fonts gnu-netcat grub gptfdisk gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly gstreamer-vaapi gvfs gvfs-smb haveged hdparm hwdetect hwinfo ipw2100-fw ipw2200-fw iwd lftp libdvdcss libgsf libopenraw libwnck3 lsscsi mc meld mesa-utils mkinitcpio-archiso mkinitcpio-nfs-utils mlocate mtools nano-syntax-highlighting nbd ndisc6 neofetch networkmanager networkmanager-openvpn nfs-utils nilfs-utils nmap noto-fonts noto-fonts-cjk noto-fonts-emoji nss-mdns ntfs-3g ntp nvme-cli openconnect openssh openvpn os-prober pacman-contrib pavucontrol pipewire alsa pipewire-jack pipewire-media-session pipewire-pulse pkgfile poppler-glib pptpclient pv python-capng python-defusedxml python-packaging python-pyqt5 qemu qemu-arch-extra rebuild-detector pptpclient reflector rsync sed sg3_utils smartmontools sof-firmware sshfs tcpdump tldr ttf-anonymous-pro ttf-bitstream-vera ttf-cascadia-code ttf-dejavu ttf-droid ttf-fantasque-sans-mono ttf-fira-code ttf-fira-mono ttf-hack ttf-ibm-plex ttf-inconsolata ttf-jetbrains-mono ttf-junicode ttf-liberation ttf-liberation ttf-linux-libertine ttf-monofur ttf-opensans ttf-roboto ttf-ubuntu-font-family unrar unzip usb_modeswitch vpnc wget whois wpa_supplicant xdg-user-dirs xdg-utils xf86-input-libinput xf86-video-amdgpu xl2tpd xorg
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc --utc
+sed -i '178s/.//' /etc/locale.gen
+sed -i '398s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
+echo "LC=COLLATE=C" >> /etc/locale.conf
+echo "KEYMAP=ru >> /etc/vconsole.conf
+echo "FONT=cyr-sun16" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
 
-# Pacman -S open-vm-tools
+pacman -S grub efibootmgr efitools networkmanager network-manager-applet networkmanager-openconnect networkmanager-openvpn dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector  virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g libwnck3 mesa-utils xf86-input-libinput xf86-video-amdgpu xf86-video-ati xorg broadcom-wl-dkms dhclient ethtool iwd nss-mdns usb_modeswitch xl2tpd python-pyqt5 python-capng pacman-contrib pkgfile rebuild-detector ffmpegthumbnailer gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly libgsf libopenraw mlocate poppler-glib haveged nilfs-utils ntp smartmontools unrar unzip xz adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts cantarell-fonts freetype2 noto-fonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans alsa-firmware alsa-plugins pavucontrol wireplumber rtkit dmidecode dmraid hdparm hwdetect lsscsi sg3_utils sof-firmware power-profiles-daemon upower duf findutils fsarchiver git glances hwinfo inxi meld nano-syntax-highlighting neofetch pv python-defusedxml python-packaging tldr sed vi wget cryptsetup device-mapper diffutils dracut e2fsprogs exfatprogs f2fs-tools iptables-nft inetutils jfsutils logrotate lvm2 man-db man-pages mdadm netctl ntfs-3g perl python reiserfsprogs s-nail sysfsutils systemd-sysvcompat texinfo usbutils xfsprogs xterm
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
+systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
+systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
+
+useradd -m bgw
+echo bgw:password | chpasswd
+usermod -aG libvirt bgw
+
+echo "bgw ALL=(ALL) ALL" >> /etc/sudoers.d/bgw
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
