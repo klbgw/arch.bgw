c49f127a25f6c149e08f3dc5280ff63ef252db11
BGW <104225231+klbgw@users.noreply.github.com>
2023-07-16 22:20:43 +0300

    Update and rename Gnome.sh to KDE.sh

--- Gnome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo reflector -c Russian,Germany,France -a 12 --latest --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Sy
-sudo pacman -S --noconfirm adwaita-icon-theme eog evince file-roller firefox gdm gedit gnome-calculator gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome gnome-tweaks gnome-usage gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs nautilus nautilus-sendto sushi totem xdg-user-dirs-gtk
-sudo systemctl enable gdm
-sleep 5
-sudo reboot
---
+++ KDE.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+sudo pacman -S --noconfirm ark bluedevil breeze-gtk dolphin gwenview kcalc kdeconnect kate kde-gtk-config khotkeys kinfocenter kinit kio-fuse konsole kscreen kwallet-pam okular plasma-desktop sddm plasma-desktop plasma-disks plasma-nm plasma-pa powerdevil print-manager sddm-kcm solid  spectacle xsettingsd firefox simplescreenrecorder vlc
+sudo pacman -S --noconfirm --needed plasma kde-applications
+
+sudo systemctl enable sddm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+reboot
+
+
