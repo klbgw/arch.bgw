6cd3edda08de48cbfdada27509e17eb8c78f92af
BGW <104225231+klbgw@users.noreply.github.com>
2023-07-16 18:44:54 +0300

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -15,7 +15,14 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S grub efibootmgr efitools networkmanager network-manager-applet networkmanager-openconnect networkmanager-openvpn dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector  virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g libwnck3 mesa-utils xf86-input-libinput xf86-video-amdgpu xf86-video-ati xorg broadcom-wl-dkms dhclient ethtool iwd nss-mdns usb_modeswitch xl2tpd python-pyqt5 python-capng pacman-contrib pkgfile rebuild-detector ffmpegthumbnailer gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly libgsf libopenraw mlocate poppler-glib haveged nilfs-utils ntp smartmontools unrar unzip  adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts cantarell-fonts freetype2 noto-fonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans alsa-firmware alsa-plugins pavucontrol wireplumber rtkit dmidecode dmraid hdparm hwdetect lsscsi sg3_utils sof-firmware power-profiles-daemon upower duf fsarchiver glances hwinfo inxi meld nano-syntax-highlighting neofetch pv python-defusedxml python-packaging tldr vi wget dracut exfatprogs f2fs-tools  inetutils jfsutils logrotate lvm2 man-db man-pages mdadm netctl ntfs-3g python reiserfsprogs s-nail sysfsutils usbutils xfsprogs xterm
+sed -i '90s/.//' /etc/pacman.conf
+sed -i '91s/.//' /etc/pacman.conf
+
+pacman -S reflector
+
+reflector --country Germany,France,Russia,'United States' --age 12 --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+pacman -S grub efibootmgr efitools networkmanager network-manager-applet networkmanager-openconnect networkmanager-openvpn dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g libwnck3 mesa-utils xf86-input-libinput xf86-video-amdgpu xf86-video-ati xorg broadcom-wl-dkms dhclient ethtool iwd nss-mdns usb_modeswitch xl2tpd python-pyqt5 python-capng pacman-contrib pkgfile rebuild-detector ffmpegthumbnailer gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly libgsf libopenraw mlocate poppler-glib haveged nilfs-utils ntp smartmontools unrar unzip  adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts cantarell-fonts freetype2 noto-fonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans alsa-firmware alsa-plugins pavucontrol wireplumber rtkit dmidecode dmraid hdparm hwdetect lsscsi sg3_utils sof-firmware power-profiles-daemon upower duf fsarchiver glances hwinfo inxi meld nano-syntax-highlighting neofetch pv python-defusedxml python-packaging tldr vi wget dracut exfatprogs f2fs-tools  inetutils jfsutils logrotate lvm2 man-db man-pages mdadm netctl ntfs-3g python reiserfsprogs s-nail sysfsutils usbutils xfsprogs xterm nvim htop zsh lib32-mesa vulkan-radeon lib32-vulkan-radeon corectrl
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
 
@@ -38,6 +45,4 @@ usermod -aG libvirt bgw
 
 echo "bgw ALL=(ALL) ALL" >> /etc/sudoers.d/bgw
 
-
-printf "\e[1;32mDone! Type exit, umount all and reboot.\e[0m"
-
+printf "\e[1;32mDone! Type exit, umount -R and reboot.\e[0m"
