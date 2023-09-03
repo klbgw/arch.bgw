7587b4747261a31a8e771999fccd3b3aa0fb58d4
BGW <104225231+klbgw@users.noreply.github.com>
2023-09-03 13:40:59 +0300

    Update KDE.sh

--- KDE.sh
+++ KDE.sh
@@ -3,11 +3,7 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
-
-sudo pacman -S --noconfirm ark bluedevil breeze breeze-grub breeze-gtk drkonqi kde-gtk-config kdeplasma-addons kgamma5 kinfocenter kscreen kscreenlocker ksshaskpass ksystemstats kmenuedit kpipewire libksysguard kwin kwayland-integration kwrited plasma-browser-integration plasma-systemmonitor plasma-vault plasma-workspace plasma-workspace-wallpapers breeze-plymouth flatpak-kcm plasma-wayland-protocols plasma-wayland-session kdecoration kactivitymanagerd kde-cli-tools dolphin gwenview kcalc kdeconnect kate kinit konsole kwallet-pam okular plasma-desktop sddm plasma-desktop plasma-disks plasma-nm plasma-pa powerdevil print-manager sddm-kcm systemsettings spectacle xsettingsd firefox vlc layer-shell-qt kclock kfind kweather markdownpart skanpage sweeper blinken kalgebra kmplot kanagram kbruch kgeography ktouch kwordquiz parley minuet kbreakout kpat ksudoku kcolorchooser koko kruler svgpart ffmpegthumbs elisa kio kio-extras kio-gdrive kio-zeroconf krdc extra-cmake-modules krusader	kalendar remmina libvncserver freerdp
+sudo pacman -S --noconfirm sddm sddm-kcm audiocd-kio bluedevil kde-gtk-config kdeplasma-addons kgamma5 kinfocenter kmenuedit kscreen kscreenlocker ksshaskpass ksysguard libksysguard kwallet-pam kwayland-integration kwin qt5-virtualkeyboard  kwrited milou plasma-desktop plasma-desktop plasma-nm plasma-pa plasma-workspace plasma-workspace-wallpapers powerdevil systemsettings plasma-browser-integration gnome-themes-standard breeze-gtk plymouth ark dolphin dolphin-plugins filelight kate kcalc keditbookmarks kfind kdeconnect okular kget libktorrent kdenetwork-kget kdenetwork-filesharing khelpcenter khotkeys kinit kio-extras konsole ksystemlog kwalletmanager gwenview partitionmanager print-manager skanlite spectacle kdegraphics-thumbnailers ffmpegthumbs kimageformats qt5-imageformats kaccounts-providers poppler-data elisa dmidecode firefox plasma-systemmonitor powertop systemd-kcm plymouth-kcm kruler krdc svgpart sweeper extra-cmake-modules krusader skanpage kclock vlc kwordquiz parley minuet kcolorchooser kio-gdrive kio-zeroconf remmina libvncserver freerdp plasma-disks kdecoration kactivitymanagerd kde-cli-tools flatpak
 
 sudo sed -i '33s/.//' /etc/pacman.conf
 sudo sed -i '36s/.//' /etc/pacman.conf
